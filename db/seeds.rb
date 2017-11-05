5c61bf5364a374d5fe860c7b63c89eac046bd898
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-11-05 15:33:59 -0500

    enabled sessions and seeded DB

---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+sixers_players = [Player.create(name: "Ben"),
+Player.create(name: "Joel"),
+Player.create(name: "Markelle"),
+Player.create(name: "TJ"),
+Player.create(name: "RoCo")]
+
+celtics_players = [Player.create(name: "Kyrie"),
+Player.create(name: "Marcus"),
+Player.create(name: "Jaylen"),
+Player.create(name: "Al"),
+Player.create(name: "Jayson")]
+
+
+sixers = Team.create(team_name: "Sixers")
+celtics = Team.create(team_name: "Celtics")
+
+
+sixers.coach = Coach.create(name: "Brett Brown")
+celtics.coach = Coach.create(name: "Brad Stevens")
+
+sixers_players.each do |player|
+  player.team = sixers
+  player.save
+end
+
+celtics_players.each do |player|
+  player.team = celtics
+  player.save
+end
+
+sixers.save
+celtics.save
