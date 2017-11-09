222650be16558ba42c1ea831e56c5641aa3361d4
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-11-09 11:29:48 -0500

    added method to calculate team stats

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,17 @@
 class Team < ActiveRecord::Base
   belongs_to :coach
   has_many :players
+
+  def team_stats
+    players = self.players
+    points = 0
+    assists = 0
+    rebounds = 0
+    players.each do |player|
+      points += player.points
+      assists += player.assists
+      rebounds += player.rebounds
+    end
+    {points, assists, rebounds}
+  end
 end
