ae2725371516a7f4b9e78feb2da7192c79b45693
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-11-09 21:58:07 -0500

    trying to establish to heroku db

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,15 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection({
-    :adapter => 'postgresql',
-     :database => 'basketball_postgresql',
-     :host => 'localhost',
-     :port => '5432',
-     :username => 'Ryan',
-     :password => ''}
+db = URI.parse(ENV['HEROKU_POSTGRESQL_COBALT_URL'] || 'postgres://localhost:5432/basketball_postgresql')
+
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
 )
 
 require_all 'app'
